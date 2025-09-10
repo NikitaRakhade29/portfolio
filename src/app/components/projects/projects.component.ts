41299e0568edd9dc39a4f2c1d6348d9c8a28bb01
NikitaRakhade <nikitarakhade009@gmail.com>
2025-09-10 15:40:20 +0530

    master

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -10,12 +10,17 @@ export class ProjectsComponent {
     {
       title: 'YouTube Comment Sentiment Analysis',
       description: 'Built with Python, NLP, and Machine Learning to analyze YouTube comments. Achieved 90% accuracy using Scikit-learn.',
-      image: 'assets/youtube-sentiment.png'
+      image: 'assets/youtube_sentiment.png'
     },
     {
       title: 'FindPerfectNest (MERN)',
       description: 'A real estate platform with interactive maps, property search, and live chat between agents and users.',
-      image: 'assets/findperfectnest.png'
+      image: 'assets/find_perfect_nest.png'
+    },
+    {
+      title: 'Home Loan Finance (Spring Boot, Angular, MySQL)',
+      description: 'Developed a full-stack loan management system with role-based dashboards. Implemented workflows for loan application, CIBIL check, document verification, sanction generation, and final disbursement.',
+      image: 'assets/home_loan_finance.png'
     }
   ];
 }
